77e5a8448881e68aa8c3829227259d7cd475f43f
Alexander Oblovatniy <oblovatniy@gmail.com>
2014-12-04 21:30:41 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -15,14 +15,17 @@ Isn't this better than `regex.compile('\\\\$\\\\d\\\\d(\\\\.\\\\d\\\\d)?')`?
 
 from setuptools import setup
 
+
 setup(
     name='rere',
     version='0.1.0',
+    description='regex redone',
+    long_description=__doc__,
+    keywords=['re', 'regex', 'regular', 'expression', ],
     url='http://www.maleagrubb.com/rere',
     license='Apache 2',
     author='Malea Grubb',
     author_email='maleangrubb@gmail.com',
-    description='regex redone',
-    long_description=__doc__,
     py_modules=['rere'],
+    platforms=['any'],
 )
